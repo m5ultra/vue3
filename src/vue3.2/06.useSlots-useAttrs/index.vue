24ff7ced1615127381f45402a7ff89be5f487194
Dendi <352671309@qq.com>
2022-09-03 14:47:43 +0800

    Feat: useSlots use Attrs

---
+++ src/vue3.2/06.useSlots-useAttrs/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <h2>useSlots</h2>
+  <Info class="test" :data="data">
+    <template #default>
+      <p>默认插槽</p>
+    </template>
+
+    <template #header>
+      <p>Header插槽</p>
+    </template>
+
+    <template #footer="{ list }">
+      <p>Footer插槽</p>
+      <h3 v-for="{ id, content } in list" :key="id">
+        作用域插槽: {{ id }}, {{ content }}
+      </h3>
+    </template>
+  </Info>
+</template>
+
+<script setup lang="ts">
+import Info from './info.vue'
+const data = ref([{ name: 'Dendi', age: 88 }])
+onMounted(() => {})
+</script>
---
+++ src/vue3.2/06.useSlots-useAttrs/info.vue
@@ -0,0 +1,33 @@
+<template>
+  <h2>Child of info</h2>
+  <slot></slot>
+
+  <slot name="header"></slot>
+
+  <!-- <slot name="footer" :list="list"></slot> -->
+  <slot name="footer" :list="list"></slot>
+</template>
+
+<script setup lang="ts">
+const slots = useSlots()
+const attrs = useAttrs()
+const list: { id: string; content: number }[] = [
+  {
+    id: '1',
+    content: 1,
+  },
+  {
+    id: '2',
+    content: 1,
+  },
+]
+onMounted(() => {
+  console.log('slots-default', slots.default && slots.default())
+  console.log('slots-header', slots.header && slots.header())
+  // TODO 如果调用了slots.footer(),  <slot name="footer" :list="list"></slot> 标签上传递的 :list=list 会失效
+  // 可以使用这种方法代替: slots.footer({ list })
+  console.log('slots-footer', slots.footer && slots.footer({ list }))
+
+  console.log('attrs', attrs)
+})
+</script>
